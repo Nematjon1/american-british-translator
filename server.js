dfc51f828309999b44a47195ab5dda4b70d9ddfd
Nematjon <frank.sport.93@gmail.com>
2021-02-15 10:31:01 +0300

    Update server.js with other routes and listen method.

--- server.js
+++ server.js
@@ -15,3 +15,42 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  }); 
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+// User routes
+userRoutes(app);
+    
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+const portNum = process.env.PORT || 3000;
+
+// Start our server and tests!
+app.listen(portNum, () => {
+  console.log(`Listening on port ${portNum}`);
+  if (process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch (error) {
+        console.log('Tests are not valid:');
+        console.error(error);
+      }
+    }, 3500);
+  }
+});
+
+module.exports = app; // For testing
